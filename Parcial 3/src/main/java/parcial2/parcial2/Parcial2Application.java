839755445a7993dd5f4c765371a13306a2b2f519
Daniel Perdomo <daekdj@gmail.com>
2023-12-01 23:00:24 -0500

    Conexion a la bd

--- Parcial 3/src/main/java/parcial2/parcial2/Parcial2Application.java
+++ Parcial 3/src/main/java/parcial2/parcial2/Parcial2Application.java
@@ -2,6 +2,13 @@ package parcial2.parcial2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Parcial2Application {
@@ -11,3 +18,29 @@ public class Parcial2Application {
 	}
 
 }
+
+@RestController
+class MyController {
+
+    @GetMapping("/test")
+    public String testDatabaseConnection() {
+        try {
+            // Establecer conexión a MariaDB
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mariadb://localhost:3306/nombredebd", "usuario", "contrasena");
+
+            // Verificar la conexión exitosa
+            if (connection != null) {
+                return "Conexión exitosa a la base de datos";
+            } else {
+                return "Error al conectar a la base de datos";
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Error al conectar a la base de datos: " + e.getMessage();
+        }
+    }
+
+}
+
